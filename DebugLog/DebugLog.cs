5cdf7c73d1bc6d17c59a94b14c00e5b63772c567
olivegogo <olivegogo@163.com>
2014-08-27 15:33:30 +0800

    DebugLog.dll初始化

---
+++ DebugLog/DebugLog.cs
@@ -0,0 +1,257 @@
+﻿//#if OliverDebug
+#define useWriteFileLog
+//#endif
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+
+public class DebugLog : MonoBehaviour//SingletonComponent<DebugLog> 
+{
+	public enum LogType
+	{
+		Normal,
+		Warning,
+		Error,
+		Num
+	}
+	private static bool[] CheckShowByLogType = new bool[(int)LogType.Num];
+	private static string outStr;
+	private static int lineCount = 0;
+	private static bool m_bIsInit = false;
+	public void Awake()
+	{
+		Init();
+	}
+	public static void Init()
+	{
+		if (!m_bIsInit)
+		{
+			m_bIsInit = true;
+			for (int i = 0; i < CheckShowByLogType.Length; ++i)
+			{
+				CheckShowByLogType[i] = true;
+			}
+		}
+	}
+	private static bool m_bIsEnable = false;
+	public static void SetEnableState(bool isEnable)
+	{
+		m_bIsEnable = isEnable;
+
+	}
+
+	public static void SetShowType(LogType type, bool isShow)
+	{
+		CheckShowByLogType[(int)type] = isShow;
+	}
+
+	//[Conditional("OliverDebug")]
+	static void LogOut(string str, LogType type)
+	{
+		Init();
+
+		if (!CheckShowByLogType[(int)type])
+			return;
+		ClearStr();
+
+		outStr += "<=========" + lineCount + "=========>\n" + str + "\n";
+		lineCount++;
+
+		//str = string.Format("[{0:0.00}]{1}", Time.realtimeSinceStartup, str);
+//#if OliverDebug
+		if (m_bIsEnable)
+		{
+			if (type == LogType.Normal)
+				UnityEngine.Debug.Log(str);
+			else if (type == LogType.Warning)
+				UnityEngine.Debug.LogWarning(str);
+			else if (type == LogType.Error)
+				UnityEngine.Debug.LogError(str);
+		}
+//#endif
+
+#if useWriteFileLog
+		FileLogDebug.WriteLog(FLTEnum.SysLog, "[{0}:{2}]{1}", type.ToString(), str, Time.realtimeSinceStartup);
+#endif
+	}
+	static void ClearStr()
+	{
+		if (lineCount > 10)
+		{
+			outStr = "";
+			lineCount = 0;
+		}
+	}
+	public static string Bytes2Hex(byte[] bytes)
+	{
+		string rec_str = "==>";
+		for (int i = 0; i < bytes.Length; ++i)
+		{
+			rec_str += " " + String.Format("{0:X2}", bytes[i]);
+			if (i % 16 == 15)
+				rec_str += "\n==>";
+		}
+
+		return rec_str;
+	}
+
+	public static void Log(string format, params object[] args)
+	{
+		LogOut(String.Format(format, args), LogType.Normal);
+	}
+	public static void LogW(string format, params object[] args)
+	{
+		LogOut(String.Format(format, args), LogType.Warning);
+	}
+	public static void LogE(string format, params object[] args)
+	{
+		LogOut(String.Format(format, args), LogType.Error);
+	}
+	public static void FileLog(FLTEnum type, string format, params object[] args)
+	{
+		FileLogDebug.WriteLog(type, format, args);
+	}
+	public static bool isShowMessage;
+	public bool isShowMessageOnScreen = true;
+	void OnGUI()
+	{
+		isShowMessage = isShowMessageOnScreen;
+		if (isShowMessageOnScreen == true)
+		{
+			GUI.color = Color.green;
+			GUI.Label(new Rect(0, 30, 400, 700), outStr);
+		}
+	}
+}
+public enum FLTEnum
+{
+	NetLog,
+	SysLog,
+	Num,
+}
+
+public class FileLogDebug
+{
+
+	static Dictionary<FLTEnum, FileStream> fsDic = null;
+
+
+	static FileLogDebug _fileLogDebug = null;
+	public static FileLogDebug GetInstance()
+	{
+		if (_fileLogDebug == null)
+			_fileLogDebug = new FileLogDebug();
+
+		isCloseFile = false;
+
+		return _fileLogDebug;
+	}
+
+	FileLogDebug()
+	{
+#if useWriteFileLog
+		fsDic = new Dictionary<FLTEnum, FileStream>();
+#endif
+	}
+
+	FileStream OpenLogFile(FLTEnum type)
+	{
+#if UNITY_EDITOR
+		//Application.persistentDataPath
+		//Application.dataPath
+		string logpath = Application.dataPath + "/../log";
+		string filepath = logpath + "/" + type.ToString() + ".log";
+
+		if (!File.Exists(filepath))
+		{
+			Directory.CreateDirectory(logpath);
+			return new FileStream(filepath, FileMode.Create);
+		}
+		else
+		{
+			string stradd = File.GetLastWriteTime(filepath).ToString("yyyyMMdd-HHmmss");
+			string filepath2 = logpath + "/" + type.ToString() + "_" + stradd + ".log";
+			File.Copy(filepath, filepath2);
+
+			return new FileStream(filepath, FileMode.Create);
+		}
+#else
+		string logpath = Application.persistentDataPath + "/log";
+		string filepath = logpath + "/" + type.ToString() + ".log";
+
+		if (!File.Exists(filepath))
+		{
+			Directory.CreateDirectory(logpath);
+			return new FileStream(filepath, FileMode.Create);
+		}
+		else
+		{
+			//string stradd = File.GetLastWriteTime(filepath).ToString("yyyyMMdd-HHmmss");
+			//string filepath2 = logpath+"/"+ type.ToString() + "_" + stradd + ".log";
+			//File.Copy(filepath, filepath2);
+
+			return new FileStream(filepath, FileMode.Create);
+		}
+#endif
+	}
+
+	static public void WriteLog(FLTEnum type, string format, params object[] args)
+	{
+#if useWriteFileLog
+		if (GetInstance() != null)
+			GetInstance().WriteLogFile(type, String.Format(format, args));
+#endif
+	}
+
+	void WriteLogFile(FLTEnum type, string value)
+	{
+		if (isCloseFile || fsDic == null)
+			return;
+#if useWriteFileLog
+
+		//System.Console.WriteLine(value);
+
+
+		if (!fsDic.ContainsKey(type))
+		{
+			fsDic[type] = OpenLogFile(type);
+		}
+
+		if (fsDic[type] != null)
+		{
+			if (value[value.Length - 1] != '\n')
+				value += '\n';
+
+			byte[] buff_default = System.Text.Encoding.Default.GetBytes(value);
+			//string ss =  new string(System.Text.Encoding.GetEncoding("utf-8").GetChars(buff_default));
+
+			byte[] info = buff_default;
+			//byte[] info = new UTF8Encoding(true).GetBytes(value);
+			//byte[] info = new ASCIIEncoding().GetBytes(value);
+			fsDic[type].Write(info, 0, info.Length);
+
+			fsDic[type].Flush();
+
+		}
+#endif
+	}
+	private static bool isCloseFile = false;
+	static public void CloseLogFiles()
+	{
+		isCloseFile = true;
+#if useWriteFileLog
+		//for (int i = 0; i < (int)FLTEnum.Num; ++i)
+		//	fsList[i].Close();
+		foreach (KeyValuePair<FLTEnum, FileStream> ele in fsDic)
+		{
+			if (ele.Value != null)
+				ele.Value.Close();
+		}
+#endif
+	}
+
+}
